241495c94a84d374cf543c23cd739542caa7314b
Alejandro Olmedo <00097017@uca.edu.sv>
2020-04-25 18:34:56 -0600

    Bug fix!

--- tarea_2_00097017.asm
+++ tarea_2_00097017.asm
@@ -1,43 +1,4 @@
 		org 100h
-		;EJERCICIO 1
-		mov ax, 0000h
-		mov bx, 0000h
-		mov bx, 9d
-		add ax, bx
-		mov bx, 7d
-		add ax, bx
-		mov bx, 0d
-		add ax, bx
-		mov bx, 1d
-		add ax, bx
-		mov bx, 7d
-		add ax, bx
-
-		mov bx, 5d; Dividir entre 5 
-		div bx ; En ax(cociente) queda 4!
-
-		mov cl, "M"
-		mov [200h],cl
-		mov cl, "e"
-		mov [201h],cl
-		mov cl, " "
-		mov [202h],cl
-		mov cl, "r"
-		mov [203h],cl
-		mov cl, "e"
-		mov [204h],cl
-		mov cl, "c"
-		mov [205h],cl
-		mov cl, "u"
-		mov [206h],cl
-		mov cl, "p"
-		mov [207h],cl
-		mov cl, "e"
-		mov [208h],cl
-		mov cl, "r"
-		mov [209h],cl
-		mov cl, "o"
-		mov [20Ah],cl
 
 
  ;;EJERCICIO 2
@@ -59,7 +20,7 @@ bucle2: inc bx
 		inc bx
 		mul cx
 		mov [bx+210h], ax
-		cmp bx, 12d Si ; Si ya estuvieron las 11 veces
+		cmp bx, 12d ; Si ya estuvieron las 11 veces
 		jbe bucle2
 
 ;EJERCICIO 3
@@ -97,6 +58,37 @@ while2:
 		cmp bx, 10h ;Mientras no se haya llegado a 16 la iteracion sigue (hexa)
 		jb while1
 
+		;EJERCICIO 1
+section .text
+
+		mov di,0d
+		mov cx, [len]
+
+lupita: mov bl, [comnt+di]
+		mov [di+200h], bl
+		inc di
+		loop lupita
+		int 20h
+
+		mov ax, 0000h
+		mov bx, 0000h
+		mov bx, 9d
+		add ax, bx
+		mov bx, 7d
+		add ax, bx
+		mov bx, 0d
+		add ax, bx
+		mov bx, 1d
+		add ax, bx
+		mov bx, 7d
+		add ax, bx
+
+		mov bx, 5d; Dividir entre 5 
+		div bx ; En ax(cociente) queda 4!
+
+section .data
+comnt 	db "Me recupero" ; Resultado=4
+len 	equ $-comnt
 
 fin:	int 10h
 
