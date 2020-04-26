7b6b111f238731c30378bab506aabe5bcd9376a7
Alejandro Olmedo <00097017@uca.edu.sv>
2020-04-25 18:20:12 -0600

    Terminando fibo !

--- tarea_2_00097017.asm
+++ tarea_2_00097017.asm
@@ -53,15 +53,49 @@
 bucle1:	inc bx
 		mul cx
 		mov [bx+210h], ax
-		cmp ax, 0Xff ; Si ya estuvieron las 11 veces
+		cmp ax, 0Xff ; Para 
 		jb bucle1
-		inc bx
 bucle2: inc bx
 		inc bx
 		mul cx
 		mov [bx+210h], ax
-		cmp bx, 12d
+		cmp bx, 12d Si ; Si ya estuvieron las 11 veces
 		jbe bucle2
+
+;EJERCICIO 3
+		mov ax, 0000h
+		mov bx, 0000h
+		mov cx, 0000h
+		mov dx, 0000h
+
+		mov dx, 0d
+		mov [220h], dx ;Casos trivial 1
+		mov dx, 1h
+		mov [221h], dx ; Caso trivial 2
+		mov bx, 1h  ;Será mi contador para la "recursion"
+
+while1:
+		inc bx 
+		mov ax, [220h+bx-1h]  ;Funciona como variable x-1
+		mov cx, [220h+bx-2h] ; Funciona como variable x-2
+		add ax, cx
+
+		cmp ax,  0xff
+		jb while2
+
+		mov [220h+bx], ax
+		cmp bx, 10h ;Mientras no se haya llegado a 16 la iteracion sigue (hexa)
+		jb while1
+
+while2:
+		mov ah, [220h+bx] ;Cuando se excedan del numero maximo para dos bits, se usaran dos celdas
+		mov al, [220h+bx+1h]
+		inc bx
+		inc bx
+		mov ax, [220h+bx-1h]
+		mov bx, [220h+bx-3h]
+		cmp bx, 10h ;Mientras no se haya llegado a 16 la iteracion sigue (hexa)
+		jb while1
 
 
 fin:	int 10h
