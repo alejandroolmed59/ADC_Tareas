17a776f7fd59db476d0aadfdc3595c8186de6d11
alejandroolmed59 <00097017@uca.edu.sv>
2020-06-21 13:53:34 -0600

    doble linea termina3

--- tarea5/tarea_5_00097017.asm
+++ tarea5/tarea_5_00097017.asm
@@ -5,15 +5,6 @@ section .text
 	call 	grafico	; Llamada a iniciar modo grafico 13h
 
 	mov di, 0h
-horizontal1:	mov 	cx, 100d ; Columna 
-            add	cx, di	 ; Offset
-            mov	dx, 50d ; Fila
-            call 	pixel
-            inc 	di
-            cmp 	di, 50d
-            jne 	horizontal1
-
-    mov di, 0h
 vertical1:  mov cx, 125d;
             mov dx, 50d
             add dx, di
@@ -77,9 +68,53 @@ vertical4: mov cx,110d ;Columna
             inc di
             cmp di,111d
             jne vertical4
-            call fin
-        
 
+;PRIMERA BARRA
+        mov di,0h
+barra1:	mov 	cx, 80d ; Columna 
+            add	cx, di	 ; Offset
+            mov	dx, 50d ; Fila
+            call 	pixel
+            inc 	di
+            cmp 	di, 30d
+            jne 	barra1
+
+    mov di, 0h
+barra2:	mov 	cx, 125d ; Columna 
+            add	cx, di	 ; Offset
+            mov	dx, 50d ; Fila
+            call 	pixel
+            inc 	di
+            cmp 	di, 30d
+            jne 	barra2
+
+mov di,0h
+barraver1: mov cx, 80d
+           mov dx, 40d
+           add dx, di
+           call pixel
+           inc di
+           cmp di,10d
+           jne barraver1
+
+mov di,0h
+barraver2: mov cx, 155d
+           mov dx, 40d
+           add dx, di
+           call pixel
+           inc di
+           cmp di,10d
+           jne barraver2
+
+mov di,00h
+barra3: mov cx, 80d
+        add cx, di
+        mov dx, 40d
+        call pixel
+        inc di
+        cmp di, 75d
+        jne barra3
+         
 
 fin:   call 	kb	 ; Utilizamos espera de alguna tecla
 	int 	20h
