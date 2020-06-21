5dc9634afe6e0426754aa71bbc97040d2f8823a7
alejandroolmed59 <00097017@uca.edu.sv>
2020-06-21 13:27:13 -0600

    J de Jose dibujada yayy

--- tarea5/tarea_5_00097017.asm
+++ tarea5/tarea_5_00097017.asm
@@ -4,18 +4,48 @@ section .text
 
 	call 	grafico	; Llamada a iniciar modo grafico 13h
 
-	xor 	di, di
-lupi:	mov 	cx, 100d ; Columna 
-	add	cx, di	 ; Offset
-	mov	dx, 100d ; Fila
-	call 	pixel
-	inc 	di
-	cmp 	di, 100d
-	jne 	lupi
+	mov di, 0h
+horizontal1:	mov 	cx, 100d ; Columna 
+            add	cx, di	 ; Offset
+            mov	dx, 50d ; Fila
+            call 	pixel
+            inc 	di
+            cmp 	di, 50d
+            jne 	horizontal1
 
-	call 	kb	 ; Utilizamos espera de alguna tecla
+    mov di, 0h
+vertical1:  mov cx, 125d;
+            mov dx, 50d
+            add dx, di
+            call pixel
+            inc di
+            cmp di,126d ;El 126d no se dibuja
+            jne vertical1
+        
+    mov di, 0h
+horizontal2:  mov cx, 50d
+              add cx, di
+              mov dx, 175d
+              call pixel
+              inc di
+              cmp di,75d
+              jne horizontal2
 
+    mov di, 0h
+vertical2: mov cx,50d
+            mov dx, 125d ;QUIERO LLEGAR HASTA 175d
+            add dx, di
+            call pixel
+            inc di
+            cmp di,50d
+            jne vertical2
+            call fin
+
+
+
+fin:   call 	kb	 ; Utilizamos espera de alguna tecla
 	int 	20h
+
 
 grafico:mov	ah, 00h
 	mov	al, 13h
@@ -23,7 +53,7 @@ grafico:mov	ah, 00h
 	ret
 
 pixel:	mov	ah, 0Ch
-	mov	al, 1010b
+	mov	al, 0100b
 	int 	10h
 	ret
 
